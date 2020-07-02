23eee785bc487226f2a9d0a1ae4cdb89aa486892
Aaron Contreras <aaronlcaq@gmail.com>
2020-07-02 07:05:41 -0500

    Modify Node to comply with Rubocop

--- node.rb
+++ node.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
+# Represents a node in a Binary Search Tree
 class Node
   include Comparable
 
   attr_reader :value
   attr_accessor :left_child, :right_child
-  
+
   def initialize(value)
     @value = value
   end
 
-  def <=>(other_node)
-    value <=> other_node.value
+  def <=>(other)
+    value <=> other.value
   end
 end
